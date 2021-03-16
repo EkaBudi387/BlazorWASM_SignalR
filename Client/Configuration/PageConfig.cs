ff90f164a1fda792b537e436d9f71c50fd174154
Eka Budi <ekabudi387@hotmail.com>
2021-03-16 12:42:13 +0700

    add paging feature

---
+++ Client/Configuration/PageConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWASM_SignalR.Client.Configuration
+{
+    public class PageConfig
+    {
+        public bool Enabled { get; set; }
+        public int PageSize { get; set; }
+        public bool CustomPager { get; set; }
+        public int NumOfItemsToSkip(int pageNumber)
+        {
+            if (Enabled)
+            {
+                return (pageNumber - 1) * PageSize;
+            }
+            return 0;
+        }
+
+        public int NumOfItemsToTake(int totalItemsCount)
+        {
+            if (Enabled)
+            {
+                return PageSize;
+            }
+            return totalItemsCount;
+        }
+
+        public int PrevPageNumber(int currentPageNumber)
+        {
+            if (currentPageNumber > 1)
+            {
+                return currentPageNumber - 1;
+
+            }
+            else
+                return 1;
+        }
+
+        public int NextPageNumber(int currentPageNumber, int totalItemsCount)
+        {
+            if (currentPageNumber < MaxPageNumber(totalItemsCount))
+            {
+                return currentPageNumber + 1;
+            }
+            else
+            {
+                return currentPageNumber;
+            }
+        }
+
+        public int MaxPageNumber(int totalItemsCount)
+        {
+            int maxPageNumber;
+            double numberOfPages = (double)totalItemsCount / (double)PageSize;
+            if (numberOfPages == Math.Floor(numberOfPages))
+                maxPageNumber = (int)numberOfPages;
+            else
+                maxPageNumber = (int)numberOfPages + 1;
+            return maxPageNumber;
+        }
+    }
+}
